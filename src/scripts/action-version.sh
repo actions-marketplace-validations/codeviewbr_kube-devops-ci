267d52cb2e4c8bce2b7172b05dba43fe3e58fa91
Wilton Guilherme <wiltoninfra@gmail.com>
2022-07-23 20:35:18 -0300

    Update action

--- src/scripts/action-version.sh
+++ src/scripts/action-version.sh
@@ -4,7 +4,7 @@ $DOCKER_IMAGE=$@
 
 action_version () {
     echo "Update Action" >&2
-    sed -e "s;%DOCKER_IMAGE%;$DOCKER_IMAGE;g" /srv/kube-config.tmpl > /root/.kube/config
+    sed -e "s;%DOCKER_IMAGE%;$DOCKER_IMAGE;g" /srv/action.tmpl > action.yml
 }
 
 action_version
