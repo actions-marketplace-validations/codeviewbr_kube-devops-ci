67e20cd9394645427cbb93cfe41133cd4ea8af56
Wilton Guilherme <wiltoninfra@gmail.com>
2022-07-23 20:42:49 -0300

    Update action

--- src/scripts/action-version.sh
+++ src/scripts/action-version.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-$DOCKER_IMAGE=$@
+DOCKER_IMAGE="$@" 
+
+echo $DOCKER_IMAGE
 
 action_version () {
     echo "Update Action" >&2
-    sed -e "s;%DOCKER_IMAGE%;$DOCKER_IMAGE;g" /srv/action.tmpl > action.yml
+    sed -e "s;%DOCKER_IMAGE%;$DOCKER_IMAGE;g" src/templates/action.tmpl > action.yml
 }
 
 action_version
+
