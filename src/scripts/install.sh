e765a1798ea1161856d43ad6679d6b25e8be6377
Wilton Guilherme <wiltoninfra@gmail.com>
2022-07-24 00:15:59 -0300

    Update tools

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -9,6 +9,9 @@ python \
 python3-pip \
 pip \
 python-setuptools \
+apt-transport-https \
+ca-certificates \
+curl \
 groff \
 less \
 zip \
@@ -24,55 +27,56 @@ echo "install awscli"
 pip3 --no-cache-dir install --upgrade awscli
 aws --version
 
-KUBECTL=1.18.2
+KUBECTL=1.22.12
 echo "downloading kubectl ${KUBECTL}"
 curl -sL https://storage.googleapis.com/kubernetes-release/release/v${KUBECTL}/bin/linux/amd64/kubectl \
 -o /usr/local/bin/kubectl && chmod +x /usr/local/bin/kubectl
+kubectl completion bash | sudo tee /etc/bash_completion.d/kubectl > /dev/null
 kubectl version --client
 
-KUSTOMIZE=3.5.5
+KUSTOMIZE=v4.5.4
 echo "downloading kustomize ${KUSTOMIZE}"
 curl -sL https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv${KUSTOMIZE}/kustomize_v${KUSTOMIZE}_linux_amd64.tar.gz | \
 tar xz && mv kustomize /usr/local/bin/kustomize
 rm -rf kustomize_v${KUSTOMIZE}_linux_amd64.tar.gz
 kustomize version
 
-HELM_V2=2.16.7
+HELM_V2=2.17.0
 echo "downloading helm ${HELM_V2}"
 curl -sSL https://get.helm.sh/helm-v${HELM_V2}-linux-amd64.tar.gz | \
 tar xz && mv linux-amd64/helm /usr/local/bin/helm && rm -rf linux-amd64
 rm -rf helm-v${HELM_V2}-linux-amd64.tar.gz
 helm version --client
 
-HELM_V3=3.2.1
+HELM_V3=3.9.2
 echo "downloading helm ${HELM_V3}"
 curl -sSL https://get.helm.sh/helm-v${HELM_V3}-linux-amd64.tar.gz | \
 tar xz && mv linux-amd64/helm /usr/local/bin/helmv3 && rm -rf linux-amd64
 rm -rf helm-v${HELM_V3}-linux-amd64.tar.gz
 helmv3 version
 
-KUBEVAL=0.15.0
+KUBEVAL=0.16.1
 echo "downloading kubeval ${KUBEVAL}"
 curl -sL https://github.com/instrumenta/kubeval/releases/download/${KUBEVAL}/kubeval-linux-amd64.tar.gz | \
 tar xz && mv kubeval /usr/local/bin/kubeval
 rm -rf kubeval-linux-amd64.tar.gz
 kubeval --version
 
-KUBEAUDIT=0.11.5
+KUBEAUDIT=0.19.0
 echo "downloading kubeaudit ${KUBEAUDIT}"
 curl -sSL https://github.com/Shopify/kubeaudit/releases/download/v${KUBEAUDIT}/kubeaudit_${KUBEAUDIT}_linux_amd64.tar.gz | \
 tar xz && mv kubeaudit /usr/local/bin/kubeaudit
 rm -rf kubeaudit_${KUBEAUDIT}_linux_amd64.tar.gz
 kubeaudit --help
 
-CONFTEST=0.19.0
+CONFTEST=0.33.2
 echo "downloading conftest ${CONFTEST}"
 curl -sL https://github.com/open-policy-agent/conftest/releases/download/v${CONFTEST}/conftest_${CONFTEST}_Linux_x86_64.tar.gz | \
 tar xz && mv conftest /usr/local/bin/conftest
 rm -rf conftest_${CONFTEST}_Linux_x86_64.tar.gz
 conftest --version
 
-KUBESEAL=0.12.5
+KUBESEAL=0.18.1
 echo "downloading kubeseal ${KUBESEAL}"
 curl -sL https://github.com/bitnami-labs/sealed-secrets/releases/download/v${KUBESEAL}/kubeseal-linux-amd64 \
 -o /usr/local/bin/kubeseal && chmod +x /usr/local/bin/kubeseal
